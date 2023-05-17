04ecd7422465dc477b24ca55d0894df75c6ed686
Jacob Sosine <jacobsosine@gmail.com>
2023-05-17 07:39:50 -0700

    app

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p> Check out some cool stuff</p>
+        <Link to="/about">About</Link>
+    </footer>
+  )
+}
+export default Footer
